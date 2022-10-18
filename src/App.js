ee6b07daa3cf1d497cae61f52766594ba3ad58e7
Doro Siemers <dorotheasiemers@gmail.com>
2022-10-18 15:01:22 +0200

    created htlm for Header, card, navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>React and Morty</h1>
       </header>
+      <main>
+        <div>
+          <img
+            src="https://rickandmortyapi.com/api/character/avatar/2.jpegimg_avatar.png"
+            alt="Avatar"
+          />
+          <h2>Morty Smith</h2>
+        </div>
+        <nav>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </nav>
+      </main>
     </div>
   );
 }
